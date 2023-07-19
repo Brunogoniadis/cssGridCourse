ca6a612ea34605454c93997529eacb14cc25c7eb
brunogoniadis <brunogoniadis@gmail.com>
2023-07-19 18:01:42 -0300

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>The guide fo CSS</h1>
       </header>
+
+      <div className="Grid-Container">
+        <div className="item 1">1</div>
+        <div className="item 2">2</div>
+        <div className="item 3">3</div>
+        <div className="item 4">4</div>
+        <div className="item 5">5</div>
+        <div className="item 6">6</div>
+
+
+        {/*         <div className="item header"
+          style={{ backgroundColor: "#FFC500" }}
+        >header</div>
+        <div className="item main"
+          style={{ backgroundColor: "#FAD555" }}
+        >main</div>
+        <div className="item sidebar"
+          style={{ backgroundColor: "#FEE388" }}
+        >sidebar</div>
+        <div className="item footer"
+          style={{ backgroundColor: "#FFBD90" }}
+        >footer</div> */}
+
+
+      </div>
     </div>
   );
 }
